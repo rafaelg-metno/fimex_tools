a96537f8e81f271bd369bde819f8441f3813c517
Rafael Grote <rafaelg@met.no>
2018-12-12 10:23:09 +0000

    First commit

--- convert_aa_eps.sh
+++ convert_aa_eps.sh
@@ -1,8 +1,37 @@
-ptrn="/lustre/storeB/project/nwp/alertness/eps/AROME-Arctic/@YYYY@/@MM@/@DD@/@HH@/mbr@EEE@"
-cnfg="/lustre/storeB/users/rafaelk/fimex_tools/AromeGribReaderConfig.xml"
-outp="/lustre/storeB/users/rafaelk/HarmonEPS/aa_eps"
+
+#Path and pattern for grib files
+# Without EPS:
+# make_netcdf_files.sh 2015041500 2015041506 /path_to_archive/@YYYY@/@MM@/@DD@/@HH@
+# With EPS:
+# make_netcdf_files.sh 015041500 2015041506 /path_to_archive/@YYYY@/@MM@/@DD@/@HH@/@EEE@
+#(year: @YYYY@, month: @MM@, day: @DD@, hour: @HH@, ensemble member: @EEE@)
+ptrn="/lustre/storeA/users/rafaelk/PL20171127"
+
+#Path to config file:
+cnfg="/lustre/storeB/users/rafaelk/utils/fimex_tools/AromeEPSGribReaderConfigHIRLAM.xml"
+
+#Path and pattern for output files 
+#(year: @YYYY@, month: @MM@, day: @DD@, hour: @HH@, ensemble member: @EEE@)
+outp="/lustre/storeB/users/rafaelk/utils/fimex_tools/"
+
+dtg_start=2017112700		# -Start time
+dtg_end=2017112700		# -Stop time
+I=3 				# -I forecast intervall (default: 3)
+p="fc"                  	# -p prefix (default: fc)
+s="@,@_fp,@_sfx,@_full_sfx"	# -s suffix Provide suffixes as a comma separated list with leading '@' i.e. (@,@_fp,@_sfx).
+l=66				# -l prognostic length (default 66)
+o="fc@YYYY@@MM@@DD@@HH@.nc"	# -o output file (default: ${prefix}@YYYY@@MM@@DD@@HH@.nc)";
+d="./"				# -d output folder (default: ./)
+S=0				# -S skip first (default: false)
+e=0				# -e EPS (default: false)
+E=0				# -E merge EPS data into single file (default: false)
+m=10				# -m eps-members (default: 10)
+v="all"				# -v variables to extract (default: all - comma separated list)
+w="./mnf_tmp"			# -w working directory for temporary files (default: ./mnf_tmp)
+D=1				# -D Debug mode (without cleanup) (default: false)
+
 
 set -x
-./make_netcdf_files.sh 2018092000 2018092000 $ptrn -c $cnfg -d $outp -e -E -m 5 
+./make_netcdf_files.sh $dtg_start $dtg_end $ptrn -I $I -p $p -s $s -l $l -o $o -d $d -S $S -e $e -E $E -m $m -v $v -w $w -D $D
 set +x
 
--- make_netcdf_files.sh
+++ make_netcdf_files.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
+# Functions
+create_dir()
+{
+  local subdir=$1
+  # Create output directory if not existent and navigate there
+  if [ ! -d "$subdir" ]; then
+    mkdir -p "$subdir"
+  fi
+  cd $subdir
+  subdir=$(pwd) # Storing directory as absolute path
+  echo $subdir
+}
+
 # Default values
 fcint=3
 prefix="fc"
 config="AromeGribReaderConfig.xml"
 template=""
-suffix=""
+suffix="@"
 output=""
 output_dir="./"
 prog_length_in=66
-llint=1
 skip_first=0
 eps=0
 merge_eps=0
 eps_members=10
 vrb="all"
 wdir="./mnf_tmp"
-sfx=1
-atm=1
 debug=0
 
 set -- $(getopt I:i:c:p:s:S:o:d:l:e:E:m:v:w:h:f:a:D: "$@")
 while [ $# -gt 0 ]; do
   case "$1" in
     (-I) fcint=$2; shift;;
-    (-i) llint=$2; shift;;
     (-c) config=$2; shift;;
     (-p) prefix=$2; shift;;
     (-s) suffix=$2; shift;;
@@ -32,32 +41,29 @@ while [ $# -gt 0 ]; do
     (-o) output=$2; shift;;
     (-d) output_dir=$2; shift;;
     (-S) skip_first=$2; shift;;
-    (-e) eps=$2;;
-    (-E) merge_eps=$2;;
+    (-e) eps=$2; shift;;
+    (-E) merge_eps=$2; shift;;
     (-m) eps_members=$2; shift;;
     (-v) vrb="$2"; shift;;
     (-w) wdir=$2; shift;;
-    (-f) sfx=$2; shift;;
-    (-a) atm=$2; shift;;
-    (-D) debug=$2;;  
+    (-D) debug=$2; shift;;  
     (-h)
 	echo ""
 	echo "Usage: $0 DTG-START DTG-END ARCHIVE-PATTERN [-I FCINT] [-l FORECAST-LENGTH]";
 	echo ""
 	echo "Without EPS:"
-  	echo "$0 2015041500 2015041506 /work/mifata/harmonie/AM25_oper/archive/@YYYY@/@MM@/@DD@/@HH@";
+  	echo "$0 2015041500 2015041506 /path_to_archive/@YYYY@/@MM@/@DD@/@HH@";
 	echo ""
 	echo "With EPS:"
-  	echo "$0 2015041500 2015041506 /work/mifata/harmonie/AM25_oper/archive/@YYYY@/@MM@/@DD@/@HH@/@EEE@";
+  	echo "$0 2015041500 2015041506 /path_to_archive/@YYYY@/@MM@/@DD@/@HH@/@EEE@";
 	echo "";
 	echo "";	
 	echo "-I forecast intervall (default: 3)";
-        echo "-i forecast length (default: 1)";
         echo "-c config file (default: ./AromeGribReaderConfig.xml)";
-        echo "-p prefix";
-        echo "-s suffix";
+        echo "-p prefix (default: fc)";
+        echo "-s suffix Provide suffixes as a comma separated list with leading '@' i.e. (@,@_fp,@_sfx)."
         echo "-l prognostic length (default 66)";
-        echo "-o output file (default: ${prefix}@YYYY@@MM@@DD@@HH@${suffix}.nc)";
+        echo "-o output file (default: ${prefix}@YYYY@@MM@@DD@@HH@.nc)";
         echo "-d output folder (default: ./)";
         echo "-S skip first (default: false)";
 	echo "-e EPS (default: false)";
@@ -65,8 +71,6 @@ while [ $# -gt 0 ]; do
         echo "-m eps-members (default: 10)";
         echo "-v variables to extract (default: all - comma separated list)";
         echo "-w working directory for temporary files (default: ./mnf_tmp)";
-        echo "-f include surface files (default: true)";
-        echo "-a include atmosphere files (default: true)";
         echo "-D Debug mode (without cleanup) (default: false)";	
 	exit 1;;
     (--) shift; break;;
@@ -81,10 +85,10 @@ if [ "$#" -ne "3" ]; then
   echo "Usage: $0 DTG-START DTG-END ARCHIVE-PATTERN [-I FCINT] [-l FORECAST-LENGTH]";
   echo ""
   echo "Without EPS:"
-  echo "$0 2015041500 2015041506 /work/mifata/harmonie/AM25_oper/archive/@YYYY@/@MM@/@DD@/@HH@";
+  echo "$0 2015041500 2015041506 /path_to_archive/@YYYY@/@MM@/@DD@/@HH@";
   echo ""
   echo "With EPS:"
-  echo "$0 2015041500 2015041506 /work/mifata/harmonie/AM25_oper/archive/@YYYY@/@MM@/@DD@/@HH@/@EEE@";
+  echo "$0 2015041500 2015041506 /path_to_archive/@YYYY@/@MM@/@DD@/@HH@/@EEE@";
   echo ""  
   exit 1
 else
@@ -94,17 +98,22 @@ else
 fi
 
 # Default output file and folder
-# test and remove [ "$output_dir" == "" ] && output_dir="./"
-[ "$output" == "" ] && output="${prefix}@YYYY@@MM@@DD@@HH@${suffix}"
 if [ $eps -ne 1 ]; then
   $eps_members=0
 fi
+
 if [ "$vrb" == "all" ]; then
   vrbtmp="";
 else
-  #vrbtmp=$vrb
   vrbtmp=`echo $vrb | sed -e "s#,# #g"`
 fi
+
+# Conversion of suffix string to array
+sfxtmp=`echo $suffix | sed -e "s#@#grib#g"`
+sfxtmp=`echo $sfxtmp | sed -e "s#,# #g"`
+eval "sfxtmp=($sfxtmp)"
+
+[ "$output" == "" ] && output="${prefix}@YYYY@@MM@@DD@@HH@"
 
 echo ""
 echo $startDTG $endDTG $pattern
@@ -117,9 +126,8 @@ echo "Prognostic length: $prog_length_in"
 echo "Config: $config"
 [ "$skip_first" -eq 1 ] && echo "Skipping first forecast step"
 [ "$eps" -eq 1 ] && echo "EPS run with $eps_members members"
-[ "$merge_eps" -eq 1 ] && echo "Mergin EPS into single files."
-[ "$atm" -eq 1 ] && echo "Including atmosphere files."
-[ "$sfx" -eq 1 ] && echo "Including surface files."
+[ "$merge_eps" -eq 1 ] && echo "Mergin EPS into single file."
+echo "Suffixes: $suffix -> ${sfxtmp[@]}"
 echo "Extracting variables: $vrb -> $vrbtmp"
 echo "Working directory: $wdir"
 [ "$debug" -eq 1 ] && echo "Debug-mode."
@@ -149,7 +157,6 @@ while [ "$dtg" -le "$endDTG" ]; do
   
   #Setting eps variables
   mbr=0
-  
   # Loop over ensemlbe members for indexing. Only one loop in case of eps=0
   while [ "$mbr" -lt "$eps_members" ]; do
 
@@ -166,30 +173,23 @@ while [ "$dtg" -le "$endDTG" ]; do
     suboutput_dir=`echo $output_dir | sed -e "s#@YYYY@#${yyyy}#g" -e "s#@YY@#${yy}#g" -e "s#@MM@#${mm}#g" -e "s#@DD@#${dd}#g" -e "s#@HH@#${hh}#g"`
  
     # Create output directory if not existent and navigate there
-    if [ ! -d "$suboutput_dir" ]; then
-      mkdir -p "$suboutput_dir"
-    fi
-    cd $suboutput_dir
-    suboutput_dir=$(pwd) # Storing directory as absolute path
+    suboutput_dir=$(create_dir "$suboutput_dir")
     
     # Create temporary working directory
-    cd $cwd
-    if [ ! -d "$subw_dir" ]; then
-      mkdir -p "$subw_dir"
-    fi
+    subw_dir="$(create_dir $subw_dir)" # NEEDS CHECKING
     cd $subw_dir
-    subw_dir=$(pwd) # Storing directory as absolute path
 
     # Controll of set variables
     echo "Pattern: $pattern -> $subpattern";
     echo "Wdir: $wdir -> $subw_dir";
     echo "Output: $output -> $suboutput";
     echo "Output_dir: $output_dir -> $suboutput_dir";
-
-    echo "Creating symbolic links for $dtgtmp - $mbrtmp"
-    if [ $atm -eq 1 ]; then
+  
+    # Creating softlinks
+    for sfxtmp_sel in $sfxtmp; do
+      echo "Creating symbolic links for $dtgtmp - member: $mbrtmp -suffix: $sfxtmp_sel"
       # Loop through files and generating soft links to grib files
-      for f in `ls -1 $subpattern/${prefix}??????????+???grib${suffix}`; do
+      for f in `ls -1 $subpattern/${prefix}??????????+???${sfxtmp_sel}`; do  
         ff=`echo $f | awk -F + '{print $2}' | cut -c1-3`
         do=1
       
@@ -202,40 +202,16 @@ while [ "$dtg" -le "$endDTG" ]; do
 
         if [ "$do" -eq 1 ]; then
           if [ "$ff" -le "$prog_length" ]; then
-            # Linking to grib messages.
             #set -x
-            ln -s $f $suboutput.$ff.mbr$mbrtmp.grb
+            ln -s $f $suboutput.$ff.$sfxtmp_sel.mbr$mbrtmp.grb
             #set +x
           else
             echo "$ff > $prog_length No proseccing done!"
           fi
         fi
-      done
-    fi
-    if [ $sfx -eq 1 ]; then
-      # Loop through files and generating soft links to grib files
-      for f in `ls -1 $subpattern/${prefix}??????????+???grib_sfx${suffix}`; do
-        ff=`echo $f | awk -F + '{print $2}' | cut -c1-3`
-        do=1
-      
-        if [ "$ff" == "000" ]; then
-          ff=0
-          [ "$skip_first" -eq 1 ] && do=0
-        else
-          ff=`echo $ff | sed 's/^0*//'`
-        fi
-         if [ "$do" -eq 1 ]; then
-          if [ "$ff" -le "$prog_length" ]; then
-            # Linking to grib messages.
-            #set -x
-            ln -s $f $suboutput.$ff.sfx.mbr$mbrtmp.grb
-            #set +x
-          else
-            echo "$ff > $prog_length No proseccing done!"
-          fi
-        fi
-      done
-    fi
+      done 
+    done
+    
     mbr=$((mbr+=1))
     cd $cwd
     echo "";
@@ -243,109 +219,64 @@ while [ "$dtg" -le "$endDTG" ]; do
   
 
   # Setup file for eps merged run
-  if [ $eps -eq 1 -a $merge_eps -eq 1 ]; then
-
-    suboutput=`echo "$output.nc" | sed -e "s#@YYYY@#${yyyy}#g" -e "s#@YY@#${yy}#g" -e "s#@MM@#${mm}#g" -e "s#@DD@#${dd}#g" -e "s#@HH@#${hh}#g"`
-    echo "Output: $output -> $suboutput";
-
-    setup_file="$suboutput_dir/setup$dtg.cfg"
-    echo "setup_file: $setup_file"
-    
-    echo "# config file for program fimex" >> $setup_file
-    echo "[input]" >> $setup_file
-    echo "file=glob:$subw_dir/*.grb" >> $setup_file
-    echo "config=$config" >> $setup_file
-    echo "type=grib" >> $setup_file
-    mbr=0
-    while [ "$mbr" -le "$eps_members" ]; do
-      mbrtmp=$(printf "%03d" "$mbr")
-      echo "optional=memberName:mbr$mbrtmp" >> $setup_file
-      mbr=$((mbr+=1))
-    done
-
-    echo "[extract]" >> $setup_file
-    for variable in $vrbtmp; do
-     echo "selectVariables=$variable" >> $setup_file
-    done
-
-    echo "[output]" >> $setup_file
-    echo "file=$suboutput_dir/$suboutput" >> $setup_file
-    echo "type=nc4" >> $setup_file
-
-    echo "Converting..."
-    set -x
-    fimex -c $setup_file
-    set +x
-    echo ""
-
-  # Setup file for eps run 
-  elif [ $eps -eq 1 -a $merge_eps -ne 1 ]; then
-    
-    mbr=0
-    while [ "$mbr" -lt "$eps_members" ]; do
-      mbrtmp=$(printf "%03d" "$mbr")
-      
-      suboutput=`echo "$output-mbr$mbrtmp.nc" | sed -e "s#@YYYY@#${yyyy}#g" -e "s#@YY@#${yy}#g" -e "s#@MM@#${mm}#g" -e "s#@DD@#${dd}#g" -e "s#@HH@#${hh}#g"`
-      echo "Output: $output -> $suboutput";
-
-      setup_file="$suboutput_dir/setup$dtg-mbr$mbrtmp.cfg"
-      echo "setup_file: $setup_file"
-      
-      echo "# config file for program fimex" >> $setup_file
-      echo "[input]" >> $setup_file
-      echo "file=glob:$subw_dir/*mbr$mbrtmp.grb" >> $setup_file
-      echo "config=$config" >> $setup_file
-      echo "type=grib" >> $setup_file      
-
-      echo "[extract]" >> $setup_file
-      for variable in $vrbtmp; do
-        echo "selectVariables=$variable" >> $setup_file
-      done
-
-      echo "[output]" >> $setup_file
-      echo "file=$suboutput_dir/$suboutput" >> $setup_file
-      echo "type=nc4" >> $setup_file
-	
-      echo "Converting..."
-      set -x
-      fimex -c $setup_file
-      set +x
-      echo ""
-
-      mbr=$((mbr+=1))
-    done
-
-  # Setup file for other runs
-  else
-
-    suboutput=`echo "$output.nc" | sed -e "s#@YYYY@#${yyyy}#g" -e "s#@YY@#${yy}#g" -e "s#@MM@#${mm}#g" -e "s#@DD@#${dd}#g" -e "s#@HH@#${hh}#g"`
-    echo "Output: $output -> $suboutput";
-
-    setup_file="$suboutput_dir/setup$dtg.cfg"
-    echo "setup_file: $setup_file"
-    
-    echo "# config file for program fimex" >> $setup_file
-    echo "[input]" >> $setup_file
-    echo "file=glob:$subw_dir/*.grb" >> $setup_file
-    echo "config=$config" >> $setup_file
-    echo "type=grib" >> $setup_file
-    
-    echo "[extract]" >> $setup_file
-    for variable in $vrbtmp; do
-     echo "selectVariables=$variable" >> $setup_file
-    done
-
-    echo "[output]" >> $setup_file
-    echo "file=$suboutput_dir/$suboutput" >> $setup_file
-    echo "type=nc4" >> $setup_file
-
-    echo "Converting..."
-    set -x
-    fimex -c $setup_file
-    set +x
-    echo ""
+  # Resetting eps variables
+  mbr=0
   
-  fi
+  # Loop over ensemlbe members for indexing. Only one loop in case of eps=0
+  while [ "$mbr" -lt "$eps_members" ]; do
+
+    mbrtmp=$(printf "%03d" "$mbr")
+
+    if [ "$merge_eps" -eq 1 ]; then
+      # Setting mbr to eps_members to terminate after one loop
+      mbr=$eps_members
+      suboutput=`echo "$output.nc" | sed -e "s#@YYYY@#${yyyy}#g" -e "s#@YY@#${yy}#g" -e "s#@MM@#${mm}#g" -e "s#@DD@#${dd}#g" -e "s#@HH@#${hh}#g"`
+      setup_file="$suboutput_dir/setup$dtg.cfg"
+      glob_file="glob:$subw_dir/*.grb"
+    else
+echo "OUTPUT: $output-mbr$mbrtmp.nc"
+      suboutput=`echo "$output-mbr$mbrtmp.nc" | sed -e "s#@YYYY@#${yyyy}#g" -e "s#@YY@#${yy}#g" -e "s#@MM@#${mm}#g" -e "s#@DD@#${dd}#g" -e "s#@HH@#${hh}#g"`
+      setup_file="$suboutput_dir/setup$dtg-mbr$mbrtmp.cfg"
+      glob_file="glob:$subw_dir/*mbr$mbrtmp.grb"
+    fi
+
+    echo "Output: $output -> $suboutput";
+    echo "setup_file: $setup_file"
+    
+    echo "# config file for program fimex" >> $setup_file
+    echo "[input]" >> $setup_file
+
+    echo "file=$glob_file" >> $setup_file
+    echo "config=$config" >> $setup_file
+    echo "type=grib" >> $setup_file
+
+    if [ "$merge_eps" -eq 1 ]; then
+      setup_mbr=0
+      while [ "$setup_mbr" -le "$eps_members" ]; do
+        setup_mbrtmp=$(printf "%03d" "$setup_mbr") 				
+        echo "optional=memberName:mbr$setup_mbrtmp" >> $setup_file	
+        setup_mbr=$((setup_mbr+=1))							
+      done
+    fi								
+
+    echo "[extract]" >> $setup_file
+    for variable in $vrbtmp; do
+      echo "selectVariables=$variable" >> $setup_file
+    done
+
+    echo "[output]" >> $setup_file
+    echo "file=$suboutput_dir/$suboutput" >> $setup_file
+    echo "type=nc4" >> $setup_file    
+
+    echo "Converting..."
+    set -x
+    fimex -c $setup_file
+    set +x
+    echo ""
+
+    mbr=$((mbr+=1))
+  
+  done
       
   dtg=`date -u --date "$dtgtmp $fcint hours" +%Y%m%d%H`
 done
